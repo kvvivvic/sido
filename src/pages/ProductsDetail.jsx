a003a3566a3b5973ac3b8911ffcd2d97457d3d68
kvvivvic <rnjs9337@gmail.com>
2023-02-10 10:02:31 +0900

    css 수정

--- src/pages/ProductsDetail.jsx
+++ src/pages/ProductsDetail.jsx
@@ -29,14 +29,14 @@ const ProductsDetail = () => {
           <img className=" w-96 mx-auto md:w-3/4" src={image} alt={title} />
         </div>
         <div className="w-full basis-5/12 md:pl-12 flex flex-col p-4 pt-4 md:pt-24">
-          <p className=" w-20 text-center inline-block border bg-indigo-400 text-white font-bold text-sm px-2 py-1 rounded-lg">{category}</p>
+          <p className=" w-20 text-center inline-block border bg-indigo-400 text-white font-bold text-sm px-2 py-1 rounded-lg font-lato">{category}</p>
           <h2 className="text-3xl font-bold py-2 border-b border-gray-400 font-lato">{title}</h2>
           <p className="text-2xl font-bold py-2 font-lato">KRW {price}</p>
           <p className="flex items-center">{description}</p>
-          <label htmlFor="select" className=" text-indigo-400 font-bold">
+          <label htmlFor="select" className="font-lato text-indigo-400 font-bold">
             Option :
           </label>
-          <select className="p-2 m-4 border-2 border-dashed border-indigo-400 outline-none" id="select" onChange={handleSelect} value={selected}>
+          <select className="font-lato p-2 m-4 border-2 border-dashed border-indigo-400 outline-none" id="select" onChange={handleSelect} value={selected}>
             {options && options.map((option, index) => <option key={index}>{option}</option>)}
           </select>
           {success && <p className="my-2">{success}</p>}
